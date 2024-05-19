37c980f1b1aee6c01a82d403e773c68467bebd86
Tasnim Quadri <139359553+TasnimQuadri@users.noreply.github.com>
2024-05-19 19:06:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var randomNumber = Math.floor(Math.random() * 6 ) + 1;
+var randomImage = "/dice " + randomNumber +".png";
+document.querySelectorAll("img")[0].setAttribute("src" , randomImage);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "/dice " + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src" , randomImage2);
+if(randomNumber > randomNumber2){
+    document.querySelector("h1").innerHTML = "✅Player 1 Wins";
+}
+else if(randomNumber2 > randomNumber){
+document.querySelector("h1").innerHTML = "✅Player 2 Wins";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
